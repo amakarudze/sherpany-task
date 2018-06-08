f2c03e59dcd957540e995cb5d567c2d5f7f6f7c8
amakarudze <amakarudze@gmail.com>
2018-06-08 23:39:08 +0200

    Initial commit

---
+++ functional_tests.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+
+# Edina has heard about a cool new reverse geocoding address app. She goes
+# to check out its homepage.
+
+# She notices that the page title and header mentions a map and addresses.
+
+# She is invited to click on the map and get the address of the clicked
+# location.
+
+# She clicks on the map and the app looks up the address of clicked location.
+
+# She notices the page refreshes and the address is listed below on the list
+# of addresses.
+
+# She notices a marker appear on the map on the location she clicked.
+
+# She clicks again on another part of the map. This time the app cannot find
+# the address of the clicked location. The app informs her this and she notices
+# nothing is added to the list of addresses.
+
+# She again clicks on the map and notices another address added and a marker is
+# also added to the page.
+
+# She decides to reset the page and start again.
+
+# Satisfied, she closes the app to re-visit it again later.
+
