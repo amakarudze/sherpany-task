e39302749da05a4cbcd1cd821e7d31665daa614c
amakarudze <amakarudze@gmail.com>
2018-07-09 01:36:16 +0200

    Update tests

--- addresses/tests.py
+++ addresses/tests.py
@@ -21,26 +21,25 @@ class HomePageTest(TestCase):
 
 class AddressModelTest(TestCase):
     def test_string_representation(self):
-        address5 = Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
+        address2 = Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
                                           address="Herbert Chitepo Ave, Harare, Zimbabwe")
-        self.assertEqual(str(address5), address5.address)
+        self.assertEqual(str(address2), address2.address)
 
     def test_does_not_accept_unnamed_address(self):
         with self.assertRaises(ValidationError):
-            address2 = Address.objects.create(lat=-17.957160658802813, lon=31.12964037109373,
+            Address.objects.create(lat=-17.957160658802813, lon=31.12964037109373,
                                               address="Unnamed Road, Zimbabwe")
 
     def test_does_not_accept_duplicate_address(self):
-        address3 = Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
+        Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
                                           address="Herbert Chitepo Ave, Harare, Zimbabwe")
         with self.assertRaises(Exception) as raised:
-            address4 = Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
+            Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
                                               address="Herbert Chitepo Ave, Harare, Zimbabwe")
 
     def test_can_reset_database(self):
-        address1 = Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
+        address3 = Address.objects.create(lat=-17.8251657, lon=31.033510000000003,
                                           address="Herbert Chitepo Ave, Harare, Zimbabwe")
         Address.objects.all().delete()
         response = self.client.get('/')
-        self.assertNotContains(response, address1.address)
-
+        self.assertNotContains(response, address3.address)
